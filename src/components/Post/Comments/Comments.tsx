f7eaedf01645bb14b6d7f5b8f94f02edd9f6b3b8
tami <xorud7979@gmail.com>
2023-07-11 15:23:27 +0900

    test

--- src/components/Post/Comments/Comments.tsx
+++ src/components/Post/Comments/Comments.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { DiscussionEmbed } from "disqus-react";
 
@@ -10,22 +10,45 @@ interface Props {
 }
 
 const Comments: React.FC<Props> = ({ postTitle, postSlug }: Props) => {
-  const { url, disqusShortname } = useSiteMetadata();
+  const COMMENTS_ID = "comments-container";
+  // const { url, disqusShortname } = useSiteMetadata();
 
-  if (!disqusShortname) {
-    return null;
-  }
+  // if (!disqusShortname) {
+  //   return null;
+  // }
 
-  return (
-    <DiscussionEmbed
-      shortname={disqusShortname}
-      config={{
-        url: url + postSlug,
-        identifier: postTitle,
-        title: postTitle,
-      }}
-    />
-  );
+  // return (
+  //   <DiscussionEmbed
+  //     shortname={disqusShortname}
+  //     config={{
+  //       url: url + postSlug,
+  //       identifier: postTitle,
+  //       title: postTitle,
+  //     }}
+  //   />
+  // );
+  const Comments = () => {
+    useEffect(() => {
+      const toggleDark = document.body.classList.contains("dark");
+      const script = document.createElement("script");
+      script.src = "https://utteranc.es/client.js";
+      // Replace with the repo on which you configured Utterances
+      script.setAttribute("repo", "tamoimi/new-blog");
+      script.setAttribute("issue-term", "pathname");
+      script.setAttribute("theme", toggleDark? "icy-dark": "github-light"); 
+      script.setAttribute("crossorigin", "anonymous");
+      script.async = true;
+      const comments = document.getElementById(COMMENTS_ID);
+      if (comments) comments.appendChild(script);
+      return () => {
+        const comments = document.getElementById(COMMENTS_ID);
+        if (comments) comments.innerHTML = "";
+      };
+    }, []);
+    return (
+      <div id={COMMENTS_ID} />
+    );
+
 };
 
 export default Comments;
--- src/templates/PostTemplate/PostTemplate.tsx
+++ src/templates/PostTemplate/PostTemplate.tsx
@@ -7,6 +7,7 @@ import { Meta } from "@/components/Meta";
 import { Post } from "@/components/Post";
 import { useSiteMetadata } from "@/hooks";
 import { Node } from "@/types";
+import { Comments } from "@/components/Post/Comments";
 
 interface Props {
   data: {
@@ -17,7 +18,7 @@ interface Props {
 const PostTemplate: React.FC<Props> = ({ data: { markdownRemark } }: Props) => (
   <Layout>
     <Post post={markdownRemark} />
-
+    <Comments/>
   </Layout>
 );
 
